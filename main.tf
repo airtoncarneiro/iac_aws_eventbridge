c70e6e512cef1bfd369345add90a82da1d962467
Airton Carneiro <airtoncarneiro@gmail.com>
2023-06-11 12:17:40 -0300

    Cria S3 bucket e altera alguns nomes

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ provider "aws" {
   }
 }
 
-resource "aws_iam_role" "lambda_e_kinesis_role" {
-  name = "lambda_e_kinesis_role"
+resource "aws_iam_role" "lambda_and_kinesis_role" {
+  name = "lambda_and_kinesis_role"
 
   assume_role_policy = <<EOF
 {
@@ -38,21 +38,21 @@ data "archive_file" "lambda_func_payload" {
 
 
 resource "aws_iam_role_policy_attachment" "lambda_e_kinesis_policy" {
-  role       = aws_iam_role.lambda_e_kinesis_role.name
+  role       = aws_iam_role.lambda_and_kinesis_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaKinesisExecutionRole"
 }
 
 resource "aws_lambda_function" "lambda_func_payload" {
   function_name = "capture_external_post_event_to_kinesis"
   filename         = data.archive_file.lambda_func_payload.output_path
-  role             = aws_iam_role.lambda_e_kinesis_role.arn
+  role             = aws_iam_role.lambda_and_kinesis_role.arn
   handler          = "lambda_func_payload.capture_external_post_event_to_kinesis"
   source_code_hash = filebase64sha256(data.archive_file.lambda_func_payload.output_path)
   runtime = "python3.8"
 }
 
 resource "aws_api_gateway_rest_api" "api_to_lambda_func_payload" {
-  name        = "api_name"
+  name        = "api_on_gateway"
   description = "API Description"
 }
 
@@ -79,6 +79,23 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.lambda_func_payload.invoke_arn
 }
 
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_to_lambda_func_payload.id
+  stage_name  = "dev"
+
+  # Este recurso depende do resource, method e integration, então nós os incluímos no depends_on
+  depends_on = [
+    aws_api_gateway_resource.resource,
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.integration,
+  ]
+}
+
+output "invoke_url" {
+  value = "https://${aws_api_gateway_deployment.deployment.rest_api_id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}"
+}
+
+
 resource "aws_lambda_permission" "permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -87,6 +104,57 @@ resource "aws_lambda_permission" "permission" {
 
   source_arn = "${aws_api_gateway_rest_api.api_to_lambda_func_payload.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "eventbridge-handson"
+}
+
+resource "aws_iam_policy" "s3_bucket_policy" {
+  name        = "S3BucketPolicy"
+  path        = "/"
+  description = "Policy for S3 bucket policy operations"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutBucketPolicy"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "s3_bucket_policy_attachment" {
+  name       = "S3BucketPolicyAttachment"
+  roles      = [aws_iam_role.lambda_and_kinesis_role.name]
+  policy_arn = aws_iam_policy.s3_bucket_policy.arn
+}
+
+
+data "aws_iam_policy_document" "private" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:userid"
+      values   = ["*"]
+    }
+  }
+}
+
 
 # resource "aws_kinesis_stream" "kinesis_stream" {
 #   name        = "kinesis_stream"
@@ -98,8 +166,8 @@ resource "aws_lambda_permission" "permission" {
 #   destination = "extended_s3"
 
 #   extended_s3_configuration {
-#     role_arn            = aws_iam_role.lambda_e_kinesis_role.arn
-#     bucket_arn          = "arn:aws:s3:::mychallengeoneventbridge"
+#     role_arn            = aws_iam_role.lambda_and_kinesis_role.arn
+#     bucket_arn          = aws_s3_bucket.bucket.arn
 #     prefix              = "nome_prefixo/"
 #     error_output_prefix = "nome_prefixo_erro/"
 #     s3_backup_mode      = "Disabled"
