b912c25a21f0b91ea5da82989deb1341df777b48
Airton Carneiro <airtoncarneiro@gmail.com>
2023-06-10 23:58:55 -0300

    Criação da API Gateway

--- main.tf
+++ main.tf
@@ -51,33 +51,33 @@ resource "aws_lambda_function" "lambda_func_payload" {
   runtime = "python3.8"
 }
 
-# resource "aws_api_gateway_rest_api" "api" {
-#   name        = "api_name"
-#   description = "API Description"
-# }
+resource "aws_api_gateway_rest_api" "api_to_lambda_func_payload" {
+  name        = "api_name"
+  description = "API Description"
+}
 
-# resource "aws_api_gateway_resource" "resource" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-#   path_part   = "{proxy+}"
-# }
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_to_lambda_func_payload.id
+  parent_id   = aws_api_gateway_rest_api.api_to_lambda_func_payload.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-# resource "aws_api_gateway_method" "method" {
-#   rest_api_id   = aws_api_gateway_rest_api.api.id
-#   resource_id   = aws_api_gateway_resource.resource.id
-#   http_method   = "POST"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_to_lambda_func_payload.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
 
-# resource "aws_api_gateway_integration" "integration" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   resource_id = aws_api_gateway_resource.resource.id
-#   http_method = aws_api_gateway_method.method.http_method
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_to_lambda_func_payload.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
 
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.lambda_func.invoke_arn
-# }
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_func_payload.invoke_arn
+}
 
 # resource "aws_lambda_permission" "permission" {
 #   statement_id  = "AllowExecutionFromAPIGateway"
@@ -85,7 +85,7 @@ resource "aws_lambda_function" "lambda_func_payload" {
 #   function_name = aws_lambda_function.lambda_func.function_name
 #   principal     = "apigateway.amazonaws.com"
 
-#   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+#   source_arn = "${aws_api_gateway_rest_api.api_to_lambda_func_payload.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 # }
 
 # resource "aws_kinesis_stream" "kinesis_stream" {
