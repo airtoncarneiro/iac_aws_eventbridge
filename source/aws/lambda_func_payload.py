2d2f9a0050041be6c90282c8d5374c4a197437c9
Airton Carneiro <airtoncarneiro@gmail.com>
2023-06-17 12:47:05 -0300

    Lambda retornar formato AWS_PROXY para API Gateway

--- source/aws/lambda_func_payload.py
+++ source/aws/lambda_func_payload.py
@@ -1,15 +1,27 @@
+import json
+import boto3
+
 def capture_external_post_event_to_kinesis(event, context):
-    import json
-    import boto3
-    
     kinesis = boto3.client('kinesis')
     
     data_json = json.dumps(event)
     
-    response = kinesis.put_record(
-        StreamName='kinesis_stream',
-        Data=data_json,
-        PartitionKey='cpf'
-    )
+    try:
+        response = kinesis.put_record(
+            StreamName='kinesis_stream',
+            Data=data_json,
+            PartitionKey='cpf'
+        )
+        
+        status_code = response['ResponseMetadata']['HTTPStatusCode']
+        message = "Registro enviado com sucesso"
+    except Exception as e:
+        status_code = 500  # Código de erro interno do servidor
+        message = str(e)
+    
+    return {
+        "statusCode": status_code,
+        "headers": { "Content-Type": "application/json" },
+        "body": json.dumps(message)
+    }
 
-    return response
